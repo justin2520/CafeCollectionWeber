545878b23a16523af2ec534b2f41c94918f41066
Justin Weber <justinweber805@outlook.com>
2024-09-09 08:08:36 -0500

    Initial Commit

---
+++ CafeCollectionWeber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeCollectionWeber
+//
+//  Created by JUSTIN WEBER on 9/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
