a56eb8e1e37edef90bdc55975d22688731b8e7bc
Justin Weber <justinweber805@outlook.com>
2024-09-16 22:16:03 -0500

    Finished

--- CafeCollectionWeber/ViewController.swift
+++ CafeCollectionWeber/ViewController.swift
@@ -20,10 +20,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var errorLabelOutlet: UILabel!
     
+    @IBOutlet weak var passwordTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var submitPasswordButtonOutlet: UIButton!
+    
+    @IBOutlet weak var adminAreaLabelOutlet: UILabel!
+    
+    @IBOutlet weak var enterItemTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var enterPriceTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var addButtonOutlet: UIButton!
+    
+    @IBOutlet weak var deleteButtonOutlet: UIButton!
+    
     var food = ["Apple", "Orange", "Cherry", "Peach", "Pear"]
     var price = [1.00, 1.50, 1.75, 2.50, 0.50]
     var shoppingCart: [String] = []
     var quantity: [Int] = []
+    var shoppingCartPrice: [Double] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,17 +98,97 @@ class ViewController: UIViewController {
         shoppingCart.append(itemTextFieldOutlet.text!)
         quantity.append(Int(quantityTextFieldOutlet.text!)!)
         
+        for i  in 0..<food.count{
+            if food[i] == itemTextFieldOutlet.text{
+                shoppingCartPrice.append(price[i])
+            }
+        }
+        
         shoppingCartTextViewOutlet.text += itemTextFieldOutlet.text! + ": " + quantityTextFieldOutlet.text! + "\n"
         
         
         var totalCost = 0.0
+        
         for i  in 0..<shoppingCart.count{
-            totalCost += Double(quantity[i]) * price[i]
+            totalCost += Double(quantity[i]) * shoppingCartPrice[i]
         }
         
         totalPriceTextViewOutlet.text = String(totalCost)
     }
     
+//    @IBAction func passwordButtonAction(_ sender: UIButton) {
+//        if passwordTextFieldOutlet.text == "cogitoErgoSum"{
+//            passwordTextFieldOutlet.isHidden = true
+//            submitPasswordButtonOutlet.isHidden = true
+//            adminAreaLabelOutlet.isHidden = false
+//            enterItemTextFieldOutlet.isHidden = false
+//            enterPriceTextFieldOutlet.isHidden = false
+//            addButtonOutlet.isHidden = false
+//            deleteButtonOutlet.isHidden = false
+//        }
+//    }
+//    
+//    @IBAction func addButtonAction(_ sender: UIButton) {
+//        var invalidError = false
+//        
+//        for i  in food{
+//            if i == enterItemTextFieldOutlet.text{
+//                invalidError = true
+//            }
+//        }
+//        
+//        if invalidError{
+//            return
+//        }
+//        
+//        if Double(enterPriceTextFieldOutlet.text!) == nil || Double(enterPriceTextFieldOutlet.text!)! <= 0{
+//            return
+//        }
+//        
+//        
+//        food.append(enterItemTextFieldOutlet.text!)
+//        price.append(Double(enterPriceTextFieldOutlet.text!)!)
+//        
+//        listTextViewOutlet.text += food[food.count - 1] + ": " + String(price[price.count - 1]) + "\n"
+//    }
+//    
+//    @IBAction func deleteButtonAction(_ sender: UIButton) {
+//        for i  in 0..<food.count{
+//            if itemTextFieldOutlet.text == food[i]{
+//                food.remove(at: i)
+//                price.remove(at: i)
+//                
+//                itemTextFieldOutlet.text = ""
+//                
+//                for q  in 0..<food.count{
+//                    listTextViewOutlet.text += food[q] + ": " + String(price[q]) + "\n"
+//                }
+//                
+//                
+//                for r  in 0..<shoppingCart.count{
+//                    if shoppingCart[r] == food[i]{
+//                        shoppingCart.remove(at: r)
+//                        
+//                        for s  in 0..<shoppingCart.count{
+//                            shoppingCartTextViewOutlet.text += shoppingCart[s] + ": " + String(quantity[s]) + "\n"
+//                        }
+//                        
+//                        var totalCost = 0.0
+//                        
+//                        for i  in 0..<shoppingCart.count{
+//                            totalCost += Double(quantity[i]) * price[i]
+//                        }
+//                        
+//                        totalPriceTextViewOutlet.text = String(totalCost)
+//                        
+//                        break
+//                    }
+//                }
+//                
+//                break
+//            }
+//        }
+//    }
     
 
 
